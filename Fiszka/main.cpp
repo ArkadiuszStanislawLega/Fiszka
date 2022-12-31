e4881dfaf3503fc92eae7652092d1f0f76e10c26
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2022-12-31 17:53:46 +0100

    Create project - MacOS standard.

---
+++ Fiszka/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fiszka
+//
+//  Created by Arkadiusz Łęga on 31/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
