26919dd8288220d4a4055d6e1195be109dcfe9ba
Arkadiusz Lega <arkadiusz.stanislaw.lega@gmail.com>
2023-01-14 18:06:08 +0100

    Update Engine - code review.

--- Models/engine.cpp
+++ Models/engine.cpp
@@ -1,5 +1,4 @@
 #include "engine.h" 
-#include <cstdio>
 
 Engine::Engine(){ 
 	this->_db = new Database();
@@ -15,49 +14,6 @@ Engine::Engine(){
 
 	this->get_questions_number_in_series();
 	this->print_questions_series();
-}
-
-void Engine::get_questions_number_in_series(){
-	printf("Podaj ilosc pytan ktore chcesz przerobic, cyfra musi byc mniejsza od %lu:\n", this->_randomised_questions.size());
-	cin >> this->_questions_number_to_print;
-	if(this->_questions_number_to_print > this->_randomised_questions.size()){
-		printf("Nieprawidlowa wartosc. Sprobuj jeszcze raz.");
-		this->get_questions_number_in_series();
-	}
-}
-
-void Engine::print_question(){
-	Question *q = get_random_question();
-	int space = 0;
-
-	printf("%s\n", q->get_value().c_str());
-	getchar();
-	printf("%s\n", q->get_answer().c_str());
-	getchar();
-}
-
-void Engine::print_questions_series(){
-	size_t i = 0;
-	for(; i < this->_questions_number_to_print; i++){
-		this->print_question();
-	}
-}
-
-
-vector<long> Engine::get_questions_id_with_tag(){
-	vector<long> questions_ids;
-	for(auto item : this->_db->get_questions_by_tags(set<string> {this->_current_tag})){
-		questions_ids.push_back(item.first);
-	}	
-	printf("Liczba pytan (%lu) w bazie danych o tagu: #%s,", questions_ids.size(), this->_current_tag.c_str());
- 	return questions_ids;
-}
-
-void Engine::print_all_randominsed_questions(){
-	printf("Zrandomizowana lista (%lu):\n", this->_randomised_questions.size());
-	for(Question * q : this->_randomised_questions){
-		printf(" >> %s\n", q->to_string().c_str());
-	}
 }
 
 void Engine::print_tags(){
@@ -78,18 +34,6 @@ void Engine::get_tag(){
 	}
 }
 
-int Engine::get_questions_number(){
-	return this->_randomised_questions.size();
-}
-
-template <typename T>
-void Engine::quick_remove_at(std::vector<T> &v, std::size_t index){
-	if(index < v.size()){
-		v.at(index) = std::move(v.back());
-		v.pop_back();
-	} 
-}
-
 void Engine::randomise_questions(vector<long> ids){
 	int i = 0; 
 	for(i = ids.size(); i > 0; i--){
@@ -101,7 +45,57 @@ void Engine::randomise_questions(vector<long> ids){
 
 		this->quick_remove_at(ids, index);
 	}
- } 
+} 
+
+template <typename T>
+void Engine::quick_remove_at(std::vector<T> &v, std::size_t index){
+	if(index < v.size()){
+		v.at(index) = std::move(v.back());
+		v.pop_back();
+	} 
+}
+
+void Engine::print_all_randominsed_questions(){
+	printf("Zrandomizowana lista (%lu):\n", this->_randomised_questions.size());
+	for(Question * q : this->_randomised_questions){
+		printf(" >> %s\n", q->to_string().c_str());
+	}
+}
+
+vector<long> Engine::get_questions_id_with_tag(){
+	vector<long> questions_ids;
+	for(auto item : this->_db->get_questions_by_tags(set<string> {this->_current_tag})){
+		questions_ids.push_back(item.first);
+	}	
+	printf("Liczba pytan (%lu) w bazie danych o tagu: #%s,", questions_ids.size(), this->_current_tag.c_str());
+ 	return questions_ids;
+}
+
+void Engine::get_questions_number_in_series(){
+	printf("Podaj ilosc pytan ktore chcesz przerobic, cyfra musi byc mniejsza od %lu:\n", this->_randomised_questions.size());
+	cin >> this->_questions_number_to_print;
+	if(this->_questions_number_to_print > this->_randomised_questions.size()){
+		printf("Nieprawidlowa wartosc. Sprobuj jeszcze raz.");
+		this->get_questions_number_in_series();
+	}
+}
+
+void Engine::print_questions_series(){
+	size_t i = 0;
+	for(; i < this->_questions_number_to_print; i++){
+		this->print_question();
+	}
+}
+
+void Engine::print_question(){
+	Question *q = get_random_question();
+	int space = 0;
+
+	printf("%s\n", q->get_value().c_str());
+	getchar();
+	printf("%s\n", q->get_answer().c_str());
+	getchar();
+}
 
 Question * Engine::get_random_question(){
 	if(this->_randomised_questions.size()){
@@ -189,8 +183,4 @@ void Engine::fill_database(){
 				"wyswietla uspione procesy", 
 				set<string> {"linux"})
 			);
-}
-
-Database * Engine::get_database(){
-	return this->_db;
 }
--- Models/engine.h
+++ Models/engine.h
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <cstdio>
 
 #include "question.h"
 #include "database.h"
@@ -25,24 +26,22 @@ class Engine{
 		int _questions_number_to_print;
 
 		void fill_database();
-		Database * get_database();
-		vector<long> get_questions_id_with_tag();
+		void print_tags();
+		void get_tag();
 		void randomise_questions(vector<long>); 
-
 		template <typename T>
 		void quick_remove_at(vector<T> &v, size_t index);
+		void print_all_randominsed_questions();
+
+		vector<long> get_questions_id_with_tag();
+
+		void print_questions_series();
+		void print_question();
+		Question * get_random_question();
+
+		void get_questions_number_in_series();
 
 	public:
 		Engine();
-		void print_tags();
-		void print_all_randominsed_questions();
-		void get_tag();
-		void print_question();
-		void get_questions_number_in_series();
-		void print_questions_series();
-
-		Question * get_random_question();
-
-		int get_questions_number();
 };
 #endif
